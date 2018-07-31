58f270a816311397c686c7f214944f116d85c34a
miyawang <miyawang1019@gmail.com>
2018-07-31 16:42:17 +0800

    add address list

--- src/pages/member/components/form.js
+++ src/pages/member/components/form.js
@@ -1,3 +1,4 @@
+import Address from 'js/addressService.js'
 export default {
   data() {
     return {
@@ -9,12 +10,104 @@ export default {
       address: '',
       id: '',
       type: '',
-      instance:'',
+      instance: '',
+      addressData: require('js/address.json'),
+      // 省 市 区 三级
+      cityList: null,
+      districtList: null,
     }
   },
   created() {
     let query = this.$route.query;
     this.type = query.type
-    this.instance = query.instance    
+    this.instance = query.instance
+    console.log('created');
+    
+    if(this.type === 'edit') {
+      let ad = this.instance;
+      this.provinceValue = parseInt(ad.provinceValue);
+      this.name = ad.name;
+      this.tel = ad.tel;
+      this.address = ad.address;
+      console.log(this.address);
+      
+      this.id = id;
+      console.log( this.id );
+
+      console.log('created end');
+      
+      
+    }
   },
+  methods:{
+    add() {
+      let {name,tel,provinceValue,cityValue,districtValue,address} = this;
+      let data = {name,tel,provinceValue,cityValue,districtValue,address}
+      if(this.type === 'add') {
+        Address.add(data).then(res => {
+          this.$router.go(-1)
+        })
+      }
+    
+      if(this.type === 'edit') {
+        data.id = this.id
+        Address.update(data).then(res => {
+          this.$router.go(-1)
+        })
+      }
+ 
+    },
+    remove() {
+      if(window.confirm('确认删除吗？')) {
+        Address.remove(this.id).then(res => {
+          this.$router.go(-1)
+        })
+      }
+      // console.log('44444');
+    },
+    setDefault() {
+      Address.setDefault(this.id).then(res => {
+        this.$router.go(-1)
+      })
+      // console.log('55555');
+    },
+  },
+  watch: {
+    provinceValue(val) {
+      if (val === -1) {
+        return
+      }
+      let list = this.addressData.list
+      let index = list.findIndex(item => {
+        return item.value === val
+      })
+      this.cityList = list[index].children;
+      this.cityValue = -1;
+      this.districtValue = -1;
+
+      if(this.type === 'edit') {
+        this.cityValue = parseInt(this.instance.cityValue)
+        // console.log(this.cityValue);
+        
+      }
+    },
+    cityValue(val) {
+      if (val === -1) {
+        return
+      }
+      let list = this.cityList
+      let index = list.findIndex(item => {
+        return item.value === val
+      })
+      this.districtList = list[index].children;
+      this.districtValue = -1;
+
+      if(this.type === 'edit') {
+        this.districtValue = parseInt(this.instance.districtValue)
+        console.log(this.districtValue);
+        
+      }
+      // console.log('88888');
+    }
+  }
 }
